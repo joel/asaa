8af5c8f79a30b6d3e2dfb28c8fa15a87df67d71c
Joel Azemar <joel.azemar@gmail.com>
2021-07-02 17:34:52 +0200

    Update Image => Attachment

--- rename_partial_name_in_files.rb
+++ rename_partial_name_in_files.rb
@@ -3,11 +3,14 @@ to = ARGV[1]
 
 exit(1) if from.nil? || to.nil?
 
+require 'fileutils'
+
 Dir["./**/*.{rb,scss,js,erb}"].each do |file_path|
-  original_file_name = File.basename(file_path, '.*')
-  next unless original_file_name =~ /#{from}/
+  next unless file_path =~ /#{from}/
 
   puts file_path.gsub(/#{from}/, to)
 
-  File.rename(file_path, file_path.gsub(/#{from}/, to))
+  target_file_path = file_path.gsub(/#{from}/, to)
+  FileUtils.mkdir_p(File.dirname(target_file_path))
+  File.rename(file_path, target_file_path)
 end
