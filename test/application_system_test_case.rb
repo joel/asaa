f317aad48f886ded02821d489d361c761815aec8
Joel Azemar <joel.azemar@gmail.com>
2021-07-05 12:39:09 +0200

    Add Docker Headless Test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "test_helper"
-
+require "socket"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   # http://chromedriver.chromium.org/capabilities
 
@@ -11,6 +11,12 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
                           browser: :remote,
                           url: "http://host.docker.internal:9515",
                         }
+  elsif ENV["SELENIUM_REMOTE_HOST"]
+    # url = "http://#{ENV['SELENIUM_REMOTE_HOST']}:4444/wd/hub"
+    driven_by :selenium, using: :chrome, options: {
+      browser: :remote,
+      url: "http://#{IPSocket.getaddress(Socket.gethostname)}",
+      desired_capabilities: :chrome }
   elsif ENV["CHROME_HEADLESS"]
     driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: { url: ENV.fetch("HUB_URL") }
     Capybara.always_include_port = true
