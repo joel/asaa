6c19dfc96a7f621f24c2c1afd0773e06fe8dfc3c
Joel Azemar <joel.azemar@gmail.com>
2021-07-05 19:40:15 +0200

    System Tests in Docker Using a Host Browser

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -5,24 +5,29 @@ require "socket"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   # http://chromedriver.chromium.org/capabilities
 
-  if ENV["HUB_URL"]
-    Capybara.register_driver :chrome_headless do |app|
-      chrome_capabilities = ::Selenium::WebDriver::Remote::Capabilities
-                            .chrome("goog:chromeOptions" => { args: %w[
-                                      no-sandbox headless disable-gpu window-size=1400,1400
-                                    ] })
-      Capybara::Selenium::Driver.new(app,
-                                     browser: :remote,
-                                     url: ENV["HUB_URL"],
-                                     desired_capabilities: chrome_capabilities)
-    end
-    driven_by :chrome_headless
-  else
-    driven_by :selenium, using: :chrome do |driver_options|
-      driver_options.add_argument("--no-sandbox")
-      driver_options.add_argument("--disable-dev-shm-usage")
-      driver_options.add_argument("--disable-gpu")
-      driver_options.add_argument("--window-size=1400,1400")
-    end
-  end
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400],
+                       options: {
+                         browser: :remote,
+                         url: "http://host.docker.internal:9515"
+                       }
+  # if ENV["HUB_URL"]
+  #   Capybara.register_driver :chrome_headless do |app|
+  #     chrome_capabilities = ::Selenium::WebDriver::Remote::Capabilities
+  #                           .chrome("goog:chromeOptions" => { args: %w[
+  #                                     no-sandbox headless disable-gpu window-size=1400,1400
+  #                                   ] })
+  #     Capybara::Selenium::Driver.new(app,
+  #                                    browser: :remote,
+  #                                    url: ENV["HUB_URL"],
+  #                                    desired_capabilities: chrome_capabilities)
+  #   end
+  #   driven_by :chrome_headless
+  # else
+  #   driven_by :selenium, using: :chrome do |driver_options|
+  #     driver_options.add_argument("--no-sandbox")
+  #     driver_options.add_argument("--disable-dev-shm-usage")
+  #     driver_options.add_argument("--disable-gpu")
+  #     driver_options.add_argument("--window-size=1400,1400")
+  #   end
+  # end
 end
